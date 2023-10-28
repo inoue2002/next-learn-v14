975714a35813e6a791313dad9b3b73484000752c
inoue2002 <54356188+youkan2002@users.noreply.github.com>
2023-10-28 17:33:11 +0900

    feat chapter6

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import '@/app/ui/global.css';
+
+import { inter } from '@/app/ui/fonts';
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent" />
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
